6872fedd6d054b6f877b23b2e7e8e225ee60317d
Rafael <98551688+etzoider@users.noreply.github.com>
2022-12-26 17:04:57 -0300

    Updating style

---
+++ script.js
@@ -0,0 +1,26 @@
+let pencilColor = '#000';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function playAudio(url) {
+    new Audio(url).play();
+  }
+
+function openUrl() {
+    var url = 'https://github.com/etzoider?tab=repositories';
+    window.open(url);
+}
+
+
+
