f49fb4af61f2500e643b15b133c9f67c4b4410ff
Etzoider <98551688+etzoider@users.noreply.github.com>
2022-12-26 11:24:39 -0300

    Creating grid

---
+++ grid/script.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+const gridContainer = document.querySelector('.gridContainer');
+
+
+function createGrid(size, grid) { // grid of size == 3 means a grid of size (3 x 3)/ grid must be html element
+    const height = grid.offsetHeight;
+    const width = grid.offsetWidth;
+
+}
