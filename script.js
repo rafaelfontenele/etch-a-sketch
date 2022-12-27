9a635ccd297e6386abfd97d454994c1cfde8ba19
Rafael <98551688+etzoider@users.noreply.github.com>
2022-12-27 00:03:29 -0300

    Creating grid

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-let pencilColor = '#000';
+const body = document.querySelector('body');
+const grid = document.querySelector('.gridContainer');
+
+
+function createGrid(size) { /// createGrid(4) means a grid of 4 x 4
+  const gridWidth = grid.offsetWidth;
+  const squareSize = gridWidth / size;
 
 
 
+  //setting up grid rows/columns size
+  grid.style.gridTemplateColumns = `repeat(${size}, ${squareSize}px)`
+  grid.style.gridTemplateRows = `repeat(${size}, ${squareSize}px)`
+  
 
+  //adding size*size squares to grid container
+  for (let i=0;i<size**2;i++) {
+    let newSquare = document.createElement('div');
+    newSquare.classList.add('gridItem');
+    grid.appendChild(newSquare);
+  }
+  
 
-
-
-
-
-
-
+}
 
 
 
@@ -23,4 +35,4 @@ function openUrl() {
 }
 
 
-
+createGrid(12);
